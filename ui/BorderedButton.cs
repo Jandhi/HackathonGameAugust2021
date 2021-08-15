d2e3fb315aec274f8632e34f2d0d6a519d9b567e
Jandhi <jpdohrin@uwaterloo.ca>
2021-08-15 16:04:38 -0400

    Refactored buttons

---
+++ ui/BorderedButton.cs
@@ -0,0 +1,56 @@
+using System;
+using Microsoft.Xna.Framework;
+
+namespace Game.UI
+{
+    public class BorderedButton : Button
+    {
+        public BorderedButton(string text, Action action) : base(text, action, text.Length + 2, 3)
+        {
+            Draw();
+        }
+
+        public override void Draw()
+        {
+            Print(1, 1, Text);
+
+            var borderColor = IsHovered ? HoveredColor : DefaultColor;
+
+            for (var x = 0; x < Width; x++)
+            {
+                for (var y = 0; y < Height; y++)
+                {
+                    DrawCell(x, y, borderColor);
+                }
+            }
+        }
+
+        public void DrawCell(int x, int y, Color borderColor)
+        {
+            if ( x == 0 && y == 0 )
+                    {
+                        SetGlyph (x, y, 218, borderColor);
+                    }
+                    else if ( x == Width-1 && y == 0)
+                    {
+                        SetGlyph (x, y, 191, borderColor);
+                    }
+                    else if ( x== 0 && y == Height-1)
+                    {
+                        SetGlyph (x, y, 192, borderColor);
+                    }
+                    else if ( x == Width-1 && y == Height-1)
+                    {
+                        SetGlyph (x, y, 217, borderColor);
+                    }
+                    else if (y == 0 || y== Height-1)
+                    {
+                        SetGlyph (x, y, 196, borderColor);
+                    }
+                    else if (x == 0 || x== Width-1)
+                    {
+                        SetGlyph (x, y, 179, borderColor);
+                    }
+        }
+    }
+}
--- ui/Button.cs
+++ ui/Button.cs
@@ -4,25 +4,26 @@ using Microsoft.Xna.Framework;
 
 namespace Game.UI
 {
-    public class Button : SadConsole.Console
+    public class Button : SadConsole.Console, IDrawable
     {
         public string Text { get; }
-
         public Action Action { get; }
-
         public Color DefaultColor { get; }= Color.White;
+        public Color HoveredColor { get; } = Color.Red;
+        public bool IsHovered { get; set; } = false;
 
-        public Color HoveredColor { get;}= Color.Red;
-
-        public bool IsHovered {get; set;}= false;
-
-
-        public Button(string text, Action action) : base(text.Length + 2, 3)
+        public Button(string text, Action action) : base(text.Length, 1)
         {
             Action = action;
             Text = text;
+            Draw();
+        }
 
-            Print(1, 1, text);
+
+        public Button(string text, Action action, int width, int height) : base(width, height)
+        {
+            Action = action;
+            Text = text;
             Draw();
         }
 
@@ -44,44 +45,9 @@ namespace Game.UI
             Draw ();
         }
 
-        public void Draw()
+        public virtual void Draw()
         {
-
-            var borderColor = IsHovered ? HoveredColor : DefaultColor;
-
-            for (var x = 0; x < Width; x++)
-            {
-                for (var y = 0; y < Height; y++)
-                {
-                    if ( x == 0 && y == 0 )
-                    {
-                        SetGlyph (x, y, 218, borderColor);
-                    }
-                    else if ( x == Width-1 && y == 0)
-                    {
-                        SetGlyph (x, y, 191, borderColor);
-                    }
-                    else if ( x== 0 && y == Height-1)
-                    {
-                        SetGlyph (x, y, 192, borderColor);
-                    }
-                    else if ( x == Width-1 && y == Height-1)
-                    {
-                        SetGlyph (x, y, 217, borderColor);
-                    }
-                    else if (y == 0 || y== Height-1)
-                    {
-                        SetGlyph (x, y, 196, borderColor);
-                    }
-                    else if (x == 0 || x== Width-1)
-                    {
-                        SetGlyph (x, y, 179, borderColor);
-                    }
-
-                }
-
-                
-            }
+            Print(0, 0, Text, IsHovered ? HoveredColor : DefaultColor);
         }
     }
 }
